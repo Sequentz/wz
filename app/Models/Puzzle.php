c886b01afa203b73bf49e163bb05744648318879
Sequentz <wouterkvandenbossche@gmail.com>
2024-10-04 20:41:01 +0200

    #2 migrate probleem opgelost

--- app/Models/Puzzle.php
+++ app/Models/Puzzle.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Puzzle extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category_id',
+        'word_id',
+    ];
+    public function words()
+    {
+        return $this->belongsToMany(Word::class);
+    }
 }
--- app/Models/Word.php
+++ app/Models/Word.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Word extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'word',
+        'category_id',
+    ];
+    public function puzzles()
+    {
+        return $this->belongsToMany(Puzzle::class);
+    }
 }
