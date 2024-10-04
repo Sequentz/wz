2bacc7aaf7744fcf462aeb38ab657353009477bf
Sequentz <wouterkvandenbossche@gmail.com>
2024-10-04 20:15:49 +0200

    #2 tables aangemaakt

--- database/migrations/2024_10_04_181118_create_puzzles_table.php
+++ database/migrations/2024_10_04_181118_create_puzzles_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('puzzles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/migrations/2024_10_04_181130_create_words_table.php
+++ database/migrations/2024_10_04_181130_create_words_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('words', function (Blueprint $table) {
             $table->id();
+            $table->string('word');
             $table->timestamps();
         });
     }
